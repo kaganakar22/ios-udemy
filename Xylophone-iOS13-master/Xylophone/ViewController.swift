cc8d79b86094d0746c8d8c17831b82f8bd920537
kaganakar22 <kagankadripasa48@gmail.com>
2023-03-28 23:34:18 +0300

    swift deep dive function explanations

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got pressed")
+        print(sender)
+        playSound()
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+
+        }
+            
+    }
     
 
-}
+
 
