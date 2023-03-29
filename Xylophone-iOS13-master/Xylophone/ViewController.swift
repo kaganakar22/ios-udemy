5bca187e97f625b97ed6cf70d8307861ffe392e9
kaganakar22 <kagankadripasa48@gmail.com>
2023-03-29 22:26:19 +0300

    xylophone button pressed animation effect added

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -20,9 +20,18 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         print(sender)
         playSound(note: sender.currentTitle!)
-    }
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+      }
     
     func playSound(note: String) {
+        
         let url = Bundle.main.url(forResource: note, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player?.play()
