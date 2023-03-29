02e0ea4c71d72ebea9e685bc58e6aa13d4891791
kaganakar22 <kagankadripasa48@gmail.com>
2023-03-29 22:30:54 +0300

    added more animated view with UIView

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -19,15 +19,16 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         print(sender)
-        playSound(note: sender.currentTitle!)
-        //Reduces the sender's (the button that got pressed) opacity to half.
-        sender.alpha = 0.5
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+            }
 
-        //Code should execute after 0.2 second delay.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            //Bring's sender's opacity back up to fully opaque.
-            sender.alpha = 1.0
-        }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+                UIView.animate(withDuration: 0.3) {
+                    sender.alpha = 1.0
+                }
+            }
+        playSound(note: sender.currentTitle!)
       }
     
     func playSound(note: String) {
