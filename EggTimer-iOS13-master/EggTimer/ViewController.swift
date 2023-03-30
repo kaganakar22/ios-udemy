dc72f1f945eadb1cbdd317855a85f0a4a4df77cc
kaganakar22 <kagankadripasa48@gmail.com>
2023-03-30 19:06:33 +0300

    egg timer finished

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -12,14 +12,41 @@ class ViewController: UIViewController {
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
+    var timeBoil = 60
+    var timeRemain = 60
+    var timeAccum = 0
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    @IBOutlet weak var TopText: UILabel!
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         let hardness = sender.currentTitle!
-        let timeBoil = eggTimes[hardness]!
+        timeBoil = eggTimes[hardness]!
+        timeRemain = timeBoil
+        timeAccum = 0
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
         
     }
     
+    @objc func updateTimer(){
+        if timeBoil > 0{
+            print("\(timeBoil) seconds.")
+            timeBoil -= 1
+            timeAccum += 1
+            progressBar.progress = Float(timeAccum)/(Float(timeRemain))
+            
+            
+        }
+        if timeBoil == 0{
+            TopText.text = "Done"
+            timer.invalidate()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                self.TopText.text = "How do you like your eggs?"
+                
+            }
+        }
+    }
 }
-    
-   
